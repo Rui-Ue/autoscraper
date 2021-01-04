620c0f70db6d5d11e7fc437524d70d59100497ef
RuiUe <rui.ue.ds@gmail.com>
2021-01-04 13:59:28 +0900

    Add comments.

--- autoscraper/auto_scraper.py
+++ autoscraper/auto_scraper.py
@@ -10,6 +10,11 @@ from bs4 import BeautifulSoup
 
 from autoscraper.utils import get_random_str, unique_hashable, unique_stack_list, \
     ResultItem, FuzzyText, get_non_rec_text, text_match, normalize
+# import autoscraper.utils
+# autoscraper.utils.__file__
+# このローカルリポジトリの autoscraper からではなく pip でインストールしたオリジナルの autoscraper からインポートしてる。
+
+
 
 
 class AutoScraper(object):
@@ -17,36 +22,80 @@ class AutoScraper(object):
     AutoScraper : A Smart, Automatic, Fast and Lightweight Web Scraper for Python.
     AutoScraper automatically learns a set of rules required to extract the needed content
         from a web page. So the programmer doesn't need to explicitly construct the rules.
+    ↓
+    Python 用のスマートで自動で高速で軽量なウェブスクレイパー。
+    AutoScraper はウェブページから必要なコンテンツを抽出するための一連のルールを自動的に学習する。
+    そのため、プログラマは明示的にルールを構築する必要はありません。
+    まさに今回目指してる形。
 
     Attributes
     ----------
     stack_list: list
         List of rules learned by AutoScraper
+    ↓
+    学習されたルールのリスト。
+    そもそも AutoScraper はどういう「ルール」を作るのだろうか。
+    XPath なのか CSS セレクタなのか、なんだろうか。
 
     Methods
     -------
     build() - Learns a set of rules represented as stack_list based on the wanted_list,
         which can be reused for scraping similar elements from other web pages in the future.
+    ↓
+    抽出ルールの学習：
+    wanted_list (引数で渡す正解ラベル) をもとに抽出ルールを作成して stack_list 属性に格納する。
+    そのルールは、他のウェブページから同じ要素をスクレイピングするのにも使える。
+    
     get_result_similar() - Gets similar results based on the previously learned rules.
+    ↓
+    学習済みのルールによる抽出(曖昧ver)：
+    次のメソッドの対比からしてこういう感じかな。詳細はメソッド定義の docstring 
+    
     get_result_exact() - Gets exact results based on the previously learned rules.
+    ↓
+    学習済みのルールによる抽出(厳密ver)：
+    チュートリアルでも紹介されてたやつ。
+    
     get_results() - Gets exact and similar results based on the previously learned rules.
+    ↓
+    学習済みのルールによる抽出(両方ver)：
+
     save() - Serializes the stack_list as JSON and saves it to disk.
+
     load() - De-serializes the JSON representation of the stack_list and loads it back.
+    ↓
+
     remove_rules() - Removes one or more learned rule[s] from the stack_list.
+    ↓
+    学習済みルールの削除(消すものを指定)
+
     keep_rules() - Keeps only the specified learned rules in the stack_list and removes the others.
+    ↓
+    学習済みルールの削除(残すものを指定)
+
     """
 
     request_headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 \
             (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36'
     }
+    # init の外なので、インスタンス属性ではなくてクラス属性であることに注意。
+    # そのため、インスタンス生成してなくても、AutoScraper.request_headers で参照可能。
+    # ちなみに、インスタンスからも autoscraper.request_headers と参照できる。
+    # 意味は未理解。http 通信のリクエストのヘッダー情報？
+
 
     def __init__(self, stack_list=None):
         self.stack_list = stack_list or []
+        # or の面白い使い方。
+        # stack_list が None ならば右辺の [] 、None でなければ左辺の stack_list となる
+        # まあ最初からデフォルト値を stack_list=[] として self.stack_list = stack_list とした方が分かりやすい。
+
 
     def save(self, file_path):
         """
         Serializes the stack_list as JSON and saves it to the disk.
+        学習済みのルールの保存
 
         Parameters
         ----------
@@ -61,10 +110,17 @@ class AutoScraper(object):
         data = dict(stack_list=self.stack_list)
         with open(file_path, 'w') as f:
             json.dump(data, f)
+        # 辞書にした上で JSON に出力するっていう定番の方法。
+        # stack_list の形式はまだ不明。
+
 
     def load(self, file_path):
         """
         De-serializes the JSON representation of the stack_list and loads it back.
+        学習済みのルールの読み込み：
+        save() と合わせて使うのかな基本。
+        それかまあ、人手で作ったルールを読み込ませる、って風にも使えるか。
+        この機能は必要なので研究でも採用したい。
 
         Parameters
         ----------
@@ -78,50 +134,142 @@ class AutoScraper(object):
 
         with open(file_path, 'r') as f:
             data = json.load(f)
+        # JSON を dict に読み込む一般的な方法。
 
         # for backward compatibility
+        # 後方互換性(前のバージョンてこと？)のための if 文
         if isinstance(data, list):
             self.stack_list = data
             return
 
         self.stack_list = data['stack_list']
+        # ファイルから読み込んだ抽出ルールをちゃんと stack_list 属性に入れてる
+
 
     @classmethod
     def _get_soup(cls, url=None, html=None, request_args=None):
+        """
+        Input:
+          cls: クラスメソッドなので自動的にクラスオブジェクト AutoScraper が渡される(後述)。
+          url: BeautifulSoup オブジェクトに変換したいページの URL。
+          html: BeautifulSoup オブジェクトに変換したい HTML ソース。
+          request_args: Web サーバに URL で指定したページをリクエストする際のオプション?
+        Output:
+          [0]: 入力で指定された(URLの)htmlを前処理した上でBeautifulSoupインスタンスに変換したやつ
+        Note:
+          @classmethod と @staticmethod デコレータについて
+          ここには @classmethod というデコレータがついているので、これはクラスメソッド。
+          クラスメソッドとは、さっきのクラス変数 request_headers と同じ感じで、
+          クラスオブジェクト AutoScraper からもインスタンスオブジェクト autoscraper からも
+          両方から参照できるようなメソッド。
+          __init__() で自動的に第一引数にインスタンスが渡される(名前は通常self)ように、
+          クラスメソッドでは自動的に第一引数にクラスが渡される(名前は通常clsにする)。
+          そのため、↓で cls.request_headers と参照できている。
+          一方、この次のメソッドのように @staticmethod がついてると、スタティックメソッドとなる。
+          staticmethod は classmethod とほぼ同じだが、
+          唯一違うのは、staticmethod では第一引数にクラスオブジェクトが渡されないこと。
+          つまり staticmethod ではクラスオブジェクトを参照することができないこと。
+          両者の違いと使い分けについては、
+          - https://www.sejuku.net/blog/25130
+          - https://python.ms/classmethod-and-staticmethod/
+          - https://qiita.com/ysk24ok/items/848daec3886f1030f587
+        """
+
         request_args = request_args or {}
+        # デフォルト値として {} を設定したのと等価。
 
         if html:
             html = normalize(unescape(html))
             return BeautifulSoup(html, 'lxml')
+            # 引数htmlに HTML ソース文字列が渡されていた場合、
+            # unescape() で、タグとの区別のために &gt, &lt にエスケープされてた  >, < を元に戻して、`unescape("x &lt y")`
+            # utils.py 定義の normalize() でユニコード正規化等をして、
+            # BeautifulSoup オブジェクトに変換して、
+            # 返す。
 
         headers = dict(cls.request_headers)
+        # クラス属性 request_headers を headers に入れる。
         if url:
             headers['Host'] = urlparse(url).netloc
+            # 引数 url に URL が与えられてる時、
+            # URL を解析してパーツに切り分けて、FQDN(ホスト名+ドメイン名)を取得してる。https://livra.geolocation.co.jp/iplearning/250/
+            # URL の構成の勉強にもなる。具体的には↓
+            # urlparse("https://www.dtvcl.com/product/datadiver/")
+            # urlparse("https://www.dtvcl.com/download/?category=dataDiver")
+            # urlparse("https://違い.net/programing/#:~:text=「オブジェクト」とは、目,持つ「オブジェクト」の集合。")
 
         user_headers = request_args.pop('headers', {})
         headers.update(user_headers)
+        # 引数 request_args に "headers" メンバが入っていたら、
+        # それを取り出して、この関数で作った headers に追加(or上書き)する。
+
         html = requests.get(url, headers=headers, **request_args).text
         html = normalize(unescape(html))
+        # 引数の URL のページを Web サーバーにリクエストして取得して HTML ソース文字列にする。
+        # (ここまでで作ったヘッダー情報やリクエストの他のパラメータも渡しつつ)
+        # さらに、
+        # unescape() で、タグとの区別のために &gt, &lt にエスケープされてた  >, < を元に戻して、`unescape("x &lt y")`
+        # utils.py 定義の normalize() でユニコード正規化等をして、
+        # ↓でBeautifulSoup オブジェクトに変換して、
+        # 返す。
 
         return BeautifulSoup(html, 'lxml')
 
+    
     @staticmethod
     def _get_valid_attrs(item):
+        """
+        input:
+          item: 属性 item.attrs に辞書を持つようなオブジェクト (bs4関連?)
+        output:
+          [0]: item.attrs から "class", "style" メンバを取り出し、
+               もしメンバがなかったり値が空だった場合はそこを補完したような辞書。
+        Note:
+          この output をスクレイピングルールの判定に使うのか？だから形式を揃えている感じか？
+          @staticmethod については _get_soup() の docs 参照。
+        """
         key_attrs = {'class', 'style'}
         attrs = {
             k: v if v != [] else '' for k, v in item.attrs.items() if k in key_attrs
         }
+        # 辞書内包表記
+        # 原型は `{k: v for k, v in item.attrs.items()}` で、
+        # このままだと item.attrs をコピーした辞書を作るだけなので、そこに色々条件をくっつけた感じ。
+        # まず、後ろの `if k in key_attrs` によって、
+        # 「そもそも k (辞書のキー) が key_attrs("class"と"style") に含まれない時は処理しない」
+        # って感じになる。なので結局 item.attrs の "class", "style" メンバだけを処理するてこと。
+        # で、前半の `v if v != [] else ''` によって、
+        # 処理対象の v が空リスト[] の時は "" が作られてそうでない時はそのまま移される。
+        # まとめると結局「item.attrs の class, style メンバだけの辞書を作って値が[]だったら""にする」
+        # てのをやってる。
+        # 注意点としては、もし item.attrs に "class", "style" がなくてもエラーにならない。
 
         for attr in key_attrs:
             if attr not in attrs:
                 attrs[attr] = ''
+        # 先ほど item.attrs に "class" や "style" がなくて attrs にもそれが欠けてしまった場合、
+        # そこを値が空なメンバにして埋めてあげる。
         return attrs
 
+    
     @staticmethod
     def _child_has_text(child, text, url, text_fuzz_ratio):
+        """
+        input:
+          child: 多分 bs4 の要素オブジェクト(タブオブジェクト)。
+          text: テキスト
+        """
         child_text = child.getText().strip()
+        # bs4 要素(タグ)オブジェクト child から値(テキスト)を取り出して、両端の空白や改行を削除する。
+        # getText は昔のメソッド名で今は get_text らしい。
 
         if text_match(text, child_text, text_fuzz_ratio):
+            # text(文字列かREパターン?)が child のテキストと閾値 text_fuzz_ratio で類似している場合、
+            # まず、child の親要素のテキストを取り出す。
+            # もしそれが child のテキストと完全一致していたら、False をリターンする。
+            # (子じゃなくて親が text を持っている、という意味の False ?)
+            # 逆に、もしそれが chile のテキストと完全一致していなかったら、True をリターンする。(child.wanted_attr=None した上で)
+            # (親じゃなくてちゃんと子が text を持っている、という意味の True?)
             parent_text = child.parent.getText().strip()
             if child_text == parent_text:
                 return False
--- autoscraper/utils.py
+++ autoscraper/utils.py
@@ -30,21 +30,66 @@ def get_random_str(n):
 
 
 def get_non_rec_text(element):
+    """
+    input:
+      element: bs4 の要素オブジェクト
+    Output:
+      [0]: ↓で得られる str。
+    Note: 
+      まず、element.find_all によって「element の子要素(孫は含まない)の全てのテキスト」が取り出される。
+      引数 text は現在の引数 string の旧称で、recursive=False は孫は見ないよって意味。
+      https://www.crummy.com/software/BeautifulSoup/bs4/doc/#find-all
+      そしてそれを "".join() で連結させて 1 つの文字列にして、両端の空白や改行をカットする。
+    """
     return ''.join(element.find_all(text=True, recursive=False)).strip()
 
 
 def normalize(item):
+    """
+    input:
+      item: なんらかの文字列? もし文字列じゃない場合はそのまま return されるので。
+    Output:  
+      [0]: item に以下の変換処理を加えた文字列。
+           - 両端の連続する 空白(半スペ), 改行(\n), タブ(\t) を除去する。
+             例：`"\n  \n abc \t \t\t  ".strip()`
+           - 文字列の正規化 (意味は同じだが半角全角などのフォーマットが異なるやつを統一する) を行う。
+             例：`unicodedata.normalize("NFKD", "アｱ  平成㍻  1１①  ,，.．  ａa")`
+    """
     if not isinstance(item, str):
+    # item が str 型オブジェクトではない場合
         return item
+        # そのまま返す
     return unicodedata.normalize("NFKD", item.strip())
+    # NFKD形式に統一するような正規化。
 
 
 def text_match(t1, t2, ratio_limit):
+    """
+    input:
+      t1: テキスト1 (正規表現パターンオブジェクトの時もある?)
+      t2: テキスト2
+      ratio_limit: 類似度に対する閾値
+    """
     if hasattr(t1, 'fullmatch'):
+    # もし t1 が属性(メソッド)として fullmatch を持っていれば、
+    # これはつまり t1 が正規表現パターンオブジェクト (re.compile("pattern")で作られるやつ) てこと？
         return bool(t1.fullmatch(t2))
+        # 文字列 t2 の全体が、正規表現パターン t1 にマッチするかどうか、の True, False をリターン
+        # https://note.nkmk.me/python-re-regex-character-type/
     if ratio_limit >= 1:
         return t1 == t2
+        # (t1が正規表現パターンオブジェクトではなくて) ratio_limit に 1 以上が設定されてる場合、
+        # t1 と t2 が文字列として完全一致しているかどうか、の True, False をリターン
     return SequenceMatcher(None, t1, t2).ratio() >= ratio_limit
+    # 文字列 t1, t2 をゲシュタルトパターンマッチングで比較して類似度を算出。
+    # その類似度(最大1)が ratio_limit 以上であれば True, そうでなければ False を返す。
+    # (第一引数 None は比較で無視する文字列)
+    # ここに比較方法を変えれば、別のアルゴリズムになる。日本語ブログに特化した matcher 使うとか。
+    # http://pixelbeat.jp/text-matching-3-approach-with-python/#toc_id_2
+    # https://qiita.com/aizakku_nidaa/items/20abcd8aa32152786687
+    # https://blog.mudatobunka.org/entry/2016/05/08/154934
+    # https://docs.python.org/ja/3/library/difflib.html
+
 
 
 class ResultItem():
